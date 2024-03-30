3da010138d9b71c33cf705df24a76525c7b3bbfc
Ar-Ray-code <ray255ar@gmail.com>
2024-03-30 23:30:29 +0900

    update example_pkg_plugin

--- example_pkg_plugin/src/example_pkg_plugin.cpp
+++ example_pkg_plugin/src/example_pkg_plugin.cpp
@@ -5,23 +5,16 @@ namespace detector2d_plugins
 
 void DetectorExample::init(const detector2d_parameters::ParamListener & param_listener)
 {
-  params_ = param_listener.get_params();
+  this->params_ = param_listener.get_params();
+
   // TODO: Initialize the detector with the parameters
-  detector = std::make_shared<example_pkg::DetectorExample>();
+  this->detector = std::make_shared<example_pkg::DetectorExample>();
 }
 
 vision_msgs::msg::Detection2DArray DetectorExample::detect(const cv::Mat & image)
 {
   vision_msgs::msg::Detection2DArray objects;
-  detector->detect(image, objects);
-
-  if (this->params_.debug) {
-    cv::imshow("detector", this->draw_bboxes(image, objects));
-    auto key = cv::waitKey(1);
-    if (key == 27) {
-      rclcpp::shutdown();
-    }
-  }
+  this->detector->detect(image, objects);
   return objects;
 }
 }// namespace detector2d_plugins
